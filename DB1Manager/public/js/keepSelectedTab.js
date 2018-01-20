59a774626e860e56f665f40542069ef324e6320f
Darkwilli <darkwilli@t-online.de>
2018-01-20 21:40:17 +0100

    replaced html <scripts> with js files

---
+++ DB1Manager/public/js/keepSelectedTab.js
@@ -0,0 +1,19 @@
+/**
+ * To Keep the last Selected Tab active when refreshing or excecuting function(List Accounts etc.)
+ *
+ * @author mstu15
+ * @version 20.01.2017
+ */
+$(document).ready(function() {
+    if (location.hash) {
+        $("a[href='" + location.hash + "']").tab("show");
+    }
+    $(document.body).on("click", "a[data-toggle]", function(event) {
+        location.hash = this.getAttribute("href");
+    });
+});
+$(window).on("popstate", function() {
+    var anchor = location.hash || $("a[data-toggle='tab']").first().attr("href");
+    $("a[href='" + anchor + "']").tab("show");
+});
+
--- DB1Manager/resources/views/manageAccounts.blade.php
+++ DB1Manager/resources/views/manageAccounts.blade.php
@@ -86,7 +86,8 @@
                         </div>
                         <div id="list" class="tab-pane fade">
                             <h3>List All Existing Accounts</h3>
-                            <form action="listAccounts">
+                            @yield('accountList')
+                            <form action="listAccounts#list">
                                 <div class="form-group">
                                     <label for="accType">Select account type:</label>
                                     <select class="form-control" id="accType" name="accType"> 
@@ -100,7 +101,8 @@
                         </div>
                         <div id="generate" class="tab-pane fade">
                             <h3>Generate Default Login List</h3>
-                            <form action="generateLoginList">
+                            @yield('loginList')
+                            <form action="generateLoginList#generate">
                                 <div class="form-group">
                                     <label for="accType">Select account type:</label>
                                     <select class="form-control" id="accType" name="accType">
@@ -132,68 +134,6 @@
 @section('scripts')
 <script src="//cdn.bootcss.com/jquery/2.2.1/jquery.min.js"></script>
 <script src="//cdn.bootcss.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-<script>
-$(function () {
-
-    //button select all or cancel
-    $("#select-all").click(function () {
-        var all = $("input.select-all")[0];
-        all.checked = !all.checked
-        var checked = all.checked;
-        $("input.select-item").each(function (index, item) {
-            item.checked = checked;
-        });
-    });
-
-    //button select invert
-    $("#select-invert").click(function () {
-        $("input.select-item").each(function (index, item) {
-            item.checked = !item.checked;
-        });
-        checkSelected();
-    });
-
-    //button get selected info
-    $("#selected").click(function () {
-        var items = [];
-        $("input.select-item:checked:checked").each(function (index, item) {
-            items[index] = item.value;
-        });
-        if (items.length < 1) {
-            alert("no selected items!!!");
-        } else {
-            var values = items.join(',');
-            console.log(values);
-            var html = $("<div></div>");
-            html.html("selected:" + values);
-            html.appendTo("body");
-        }
-    });
-
-    //column checkbox select all or cancel
-    $("input.select-all").click(function () {
-        var checked = this.checked;
-        $("input.select-item").each(function (index, item) {
-            item.checked = checked;
-        });
-    });
-
-    //check selected items
-    $("input.select-item").click(function () {
-        var checked = this.checked;
-        console.log(checked);
-        checkSelected();
-    });
-
-    //check is all selected
-    function checkSelected() {
-        var all = $("input.select-all")[0];
-        var total = $("input.select-item").length;
-        var len = $("input.select-item:checked:checked").length;
-        console.log("total:" + total);
-        console.log("len:" + len);
-        all.checked = len === total;
-    }
-});
-</script>
+<script type="text/javascript" src="{{ URL::asset('js/selectAllCheckbox.js') }}"></script>
+<script type="text/javascript" src="{{ URL::asset('js/keepSelectedTab.js') }}"></script>
 @endsection
